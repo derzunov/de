1cd99a9dc0bd3264a1c4b93de28595e88a940686
derzunov <derzunov.work@gmail.com>
2022-10-10 01:31:13 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { client } from './client';
+import {useEffect, useState} from "react";
+
+function App() {
+
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        client.getEntries().then((response) => {
+            console.log(response);
+            setItems(response.items);
+        })
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>DE Start point</h1>
+            <h1>Elements</h1>
+            { items.map( ( item ) => { return <li>{item.fields.title}</li>}) }
+        </div>
+    );
+};
+
+export default App;
---
+++ src/client.js
@@ -0,0 +1,5 @@
+import * as contentful from "contentful"
+export const client = contentful.createClient({
+    space: "nmwuilkze0hi",
+    accessToken: "1_CyEsKx_HdJtLNw2VAyYvvaTC31sgp6E0iVcUV7T60",
+})
