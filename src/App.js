71b23aa7689269b6324e6a192f30287c7114a3a2
derzunov <derzunov.work@gmail.com>
2022-10-10 05:30:56 +0300

    котолиса)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Derzunova from "./pages/Derzunova";
 import Posts from "./pages/Posts";
+import Foxycat from "./pages/Foxycat";
 
 function App() {
 
@@ -22,11 +23,15 @@ function App() {
                 <h1>DE Start point (edited once more)</h1>
                 <h1>Elements:</h1>
                 <ul>
-                    { items.map( ( item ) => { return <li>{item.fields.title}</li>}) }
+                    { items.map( ( item ) => { return <li key= { item.sys.id }> {item.fields.title}</li>}) }
                 </ul>
                 <nav>
                     <div>
                         <Link to="/derzunova">Любовь</Link>
+                    </div>
+
+                    <div>
+                        <Link to="/foxycat">Лиса</Link>
                     </div>
 
                     <div>
@@ -40,12 +45,10 @@ function App() {
             </div>
 
             <Routes>
-                <Route path="/derzunova" element={<Derzunova/>}>
-                </Route>
-                <Route path="/posts" element={<Posts/>}>
-                </Route>
-                <Route path="/" element={<Home/>}>
-                </Route>
+                <Route path="/derzunova" element={<Derzunova/>}></Route>
+                <Route path="/foxycat" element={<Foxycat/>}></Route>
+                <Route path="/posts" element={<Posts/>}></Route>
+                <Route path="/" element={<Home/>}></Route>
             </Routes>
         </Router>
     );
